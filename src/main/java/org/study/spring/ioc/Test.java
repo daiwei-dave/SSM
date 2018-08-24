6b7a20ff80583df13faa56216068eca1c6d61963
daiwei <18428385839@163.com>
2018-08-24 18:27:18 +0800

    ioc测试

--- src/main/java/org/study/spring/ioc/Test.java
+++ src/main/java/org/study/spring/ioc/Test.java
@@ -1,5 +1,6 @@
 package org.study.spring.ioc;
 
+import org.seckill.entity.Cart;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,7 +10,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring.xml");
-        Person p=context.getBean("person",Person.class);
+//        Cart cart = context.getBean(Cart.class);
+//        if (cart==null){
+//            System.out.println("not get bean");
+//        }
+
+
+        Person p=null;
+//        p=context.getBean("person",Person.class);
+//        System.out.println(p.hashCode());
+
+        p = context.getBean(Person.class);
+        System.out.println(p.hashCode());
         p.info();
     }
 
